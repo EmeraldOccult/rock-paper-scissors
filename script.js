0a7a8c89161ac2b76ca63e8d89b19132e5978116
EmeraldOccult <212671611+EmeraldOccult@users.noreply.github.com>
2025-10-29 21:06:31 +1000

    Add functions for a scoreboard and playing a round

--- script.js
+++ script.js
@@ -17,8 +17,6 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
-
 function getHumanChoice() {
     let user = (window.prompt("Rock, Paper, or Scissors?"))
 
@@ -27,10 +25,59 @@ function getHumanChoice() {
     } else if (user.toLowerCase() === "paper") {
         return "paper";
     } else if (user.toLowerCase() === "scissors"){
-        return "scissors"
-    } else {
-        
+        return "scissors";
     }
 }
 
-console.log(getHumanChoice());
+let humanScore = 0;
+let computerScore = 0;
+
+function scoreBoard(){
+    return `Current score: You - ${humanScore} Computer - ${computerScore}`;
+}
+
+function playRound(humanChoice,computerChoice) {
+
+    if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log("You chose rock. Computer chose scissors. You win!");
+        humanScore++;
+        console.log(scoreBoard());
+        return;
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+        console.log("You chose rock. Computer chose paper. You lose!");
+        computerScore++;
+        console.log(scoreBoard());
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log("You chose paper. Computer chose rock. You win!");
+        humanScore++;
+        console.log(scoreBoard());
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        console.log("You chose paper. Computer chose scissors. You lose!");
+        computerScore++;
+        console.log(scoreBoard());
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log("You chose scissors. Computer chose paper. You win!");
+        humanScore++;
+        console.log(scoreBoard());
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        console.log("You chose scissors. Computer chose rock. You lose!");
+        computerScore++;
+        console.log(scoreBoard());
+    } else if (humanChoice === computerChoice) {
+        console.log(`You both chose ${humanChoice}. It's a tie!`);
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(computerSelection);
+console.log(humanSelection);
+
+playRound(humanSelection, computerSelection)
+
+// Note to self: humanSelection and computerSelection are variables being used as ARGUMENTS in the playRound function CALL for the PARAMETERS, humanChoice and computerChoice, in the playRound function DECLARATION //
+
+// FIX: storing scoreboard in variable scoreBoard and then console logging the variable (console.log(scoreBoard)) doesn't show incremented score. //
+// FIXED: made scoreBoard a function to call on. Problem was that console logging a variable declared before the increment doesn't update the value of the variable. //
+// tie would be humanChoice === computerChoice //
